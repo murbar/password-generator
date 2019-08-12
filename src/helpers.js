c4cdcbb9985b5507e05d9934c860fac649e0bd09
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-12 11:43:45 -0400

    Add helpers to pad and stripe arrays

--- src/helpers.js
+++ src/helpers.js
@@ -9,10 +9,20 @@ export const fireHotKey = (e, callback) => {
   }
 };
 
+export const padArray = (array, value, newLength) => {
+  const length = array.length;
+  const filler = Array(newLength - length).fill(value);
+  return array.concat(filler);
+};
+
 export const convertToCharCodeArray = string => {
   return string.split('').map(ch => ch.charCodeAt(0));
 };
 
 export const convertToString = charCodeArray => {
   return charCodeArray.map(code => String.fromCharCode(Math.floor(code))).join('');
+};
+
+export const convertToStringAndFilterType = (charCodeArray, type) => {
+  return convertToString(charCodeArray.filter(code => typeof code === type));
 };
