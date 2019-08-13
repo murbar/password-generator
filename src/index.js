38c1a3034f908fe41afb7789724928ad2153e256
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-13 19:07:50 -0400

    Add border

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import GlobalStyles from 'styles/global';
-import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import App from 'App';
+
+const AppContainer = styled.div``;
+
+const BorderBox = styled.div`
+  pointer-events: none;
+  position: fixed;
+  top: 0rem;
+  bottom: 0rem;
+  left: 0rem;
+  right: 0rem;
+  border: 0.5rem solid ${p => p.theme.colors.blue};
+  z-index: -500;
+`;
 
 const Root = () => {
   return (
     <ThemeProvider theme={theme}>
-      <>
+      <AppContainer>
+        <BorderBox />
         <GlobalStyles />
         <App />
-      </>
+      </AppContainer>
     </ThemeProvider>
   );
 };
 ReactDOM.render(<Root />, document.getElementById('root'));
-// serviceWorker.unregister();
