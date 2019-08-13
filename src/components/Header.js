a4c1fd5e1357d8a33b80199e68de7096669c477a
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-13 11:20:10 -0400

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Styles = styled.header``;
+const Styles = styled.header`
+  h1 {
+    padding: 2.5rem 0;
+    font-weight: 400;
+    line-height: 1;
+    color: #ff4dff;
+    span {
+      display: block;
+      &:first-child {
+      }
+      &:nth-child(2) {
+        font-size: 1.2em;
+      }
+      &:nth-child(3) {
+        font-size: 0.9em;
+      }
+    }
+  }
+`;
 
 export default function Header() {
   return (
     <Styles>
-      <h1>Secure password generator</h1>
+      <h1>
+        <span>Generate</span> <span>secure passwords</span>
+        <span>instantly</span>
+      </h1>
     </Styles>
   );
 }
