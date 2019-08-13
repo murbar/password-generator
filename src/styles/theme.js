aceb637c2409e808abe369b85e9fc617b26d3caa
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-13 19:04:06 -0400

    Add vars

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,23 +1,36 @@
 import { media } from './helpers';
+import config from 'config';
+
+const { strengthsEnum } = config;
 
 const colors = {
   offWhite: '#efefef',
   offBlack: '#222',
-  green: '#3cc473'
+  tomato: 'hsl(9, 100%, 70%)',
+  blue: 'hsl(195, 100%, 50%)',
+  green: 'hsl(150, 100%, 50%)',
+  fuchsia: 'hsl(300, 100%, 65%)',
+  lightGrey: '#cccccc',
+  darkGrey: '#333333'
+};
+
+const meterColors = {
+  [strengthsEnum.OK]: colors.tomato,
+  [strengthsEnum.GOOD]: colors.blue,
+  [strengthsEnum.STRONG]: colors.green
 };
 
 const theme = {
   colors: {
     ...colors,
     background: colors.offWhite,
-    foreground: colors.offBlack,
-    primary: colors.green
+    foreground: colors.offBlack
   },
+  meterColors,
   inputBorderRadius: `0.5rem`,
-  fontFamily: "'Nunito', sans-serif",
-  // fontFamilyHeadings: "'Bungee', sans-serif",
-  // fontFamilyFixed: "'Source Code Pro', sans-serif",
-  fontFamilyFixed: "'Roboto Mono', sans-serif",
+  fontFamily: "'Roboto Mono', sans-serif",
+  fontFamilyHeadings: "'Contrail One', sans-serif",
+  borderRadius: '0.2rem',
   media
 };
 
