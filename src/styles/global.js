5ec9c8a0cd9b4611e9c2496150465047809d5af0
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-13 19:05:07 -0400

    Add styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,25 +6,39 @@ export default createGlobalStyle`
   }
   html {
     font-size: 62.5%;
+    margin: 0;
   }
   body {
+    position: relative;
     margin: 0;
+    padding: 0 2rem;
     background: ${p => p.theme.colors.offWhite};
     color: ${p => p.theme.colors.offBlack};
     font-family: ${p => p.theme.fontFamily};
     font-size: 1.8rem;
     line-height: 1.7;
-    min-height: 100vh;
+    min-height: 100%;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    
   }
-  ${'' /* h1, h2, h3, h4, h5, h6 {
+  h1, h2, h3, h4, h5, h6 {
     font-family: ${p => p.theme.fontFamilyHeadings};
-  } */}
+    margin: 0;
+    font-weight: 400;
+    text-transform: uppercase;
+    color: ${p => p.theme.colors.darkGrey};
+  }
+  p{ 
+    margin: 0 0 0.5em;
+  }
+  ul, ol {
+    margin-top: 0;
+  }
   a {
     color: inherit;
     &:hover {
-      color: ${p => p.theme.colors.primary};
+      color: inherit;
     }
   }
 `;
