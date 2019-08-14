5e1e2e9f26d73766860a0f594e260d86fb633100
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-14 17:48:10 -0400

    Remove re-gen button

--- src/components/Params.js
+++ src/components/Params.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import config from 'config';
 import PasswordParams from 'components/PasswordParams';
 import PassphraseParams from 'components/PassphraseParams';
-import Button from 'components/common/Button';
-import { ReactComponent as RefreshIcon } from 'images/repeat.svg';
 import { media } from 'styles/helpers';
 
 const Styles = styled.div`
@@ -15,18 +13,9 @@ const Styles = styled.div`
     font-size: 0.8em;
   }
   ${media.tablet`
-    /* padding: 1.5rem 2rem; */
     border-radius: ${p => p.theme.borderRadius} ${p => p.theme.borderRadius} 0 0;
     margin: 0;
   `}
-`;
-
-const RegenerateButton = styled(Button)`
-  /* margin: 1.5rem; */
-  svg {
-    transform: rotate(35deg);
-    margin-right: 0.75em;
-  }
 `;
 
 export default function Params({ mode, values, onChange, reGen }) {
@@ -36,9 +25,6 @@ export default function Params({ mode, values, onChange, reGen }) {
     <Styles>
       {mode === modes.PW && <PasswordParams values={values} onChange={onChange} />}
       {mode === modes.PP && <PassphraseParams values={values} onChange={onChange} />}
-      <RegenerateButton onClick={reGen} title="Generate new secrets">
-        <RefreshIcon /> Regenerate Secrets
-      </RegenerateButton>
     </Styles>
   );
 }
