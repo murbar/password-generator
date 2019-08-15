1061a83cffc1de3244370367f5b352a51d1b5f35
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-15 11:46:04 -0400

    Add link

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -12,8 +12,15 @@ const Styles = styled.p`
 export default function Disclaimer() {
   return (
     <Styles>
-      Your secrets are generated on this device and are not transmitted or persisted in any way.
-      Just look out for nosy parkers and close this browser tab when you're done.
+      Your secrets are generated on this device with a{' '}
+      <a
+        href="https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues"
+        title="getRandomValues docs on Mozilla.org"
+      >
+        cryptographically strong random number generator
+      </a>{' '}
+      and are not transmitted or persisted in any way. Just look out for nosy parkers and
+      close this browser tab when you're done.
     </Styles>
   );
 }
