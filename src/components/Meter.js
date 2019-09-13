129fb608da3e59196830b1fb2d145bde35c10c66
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-12 20:41:22 -0400

    Clean up view logic

--- src/components/Meter.js
+++ src/components/Meter.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import styled, { withTheme } from 'styled-components';
-import config from 'config';
+import styled from 'styled-components';
 import { animated, useSpring } from 'react-spring';
+import config from 'config';
 import { media } from 'styles/helpers';
+import useTheme from 'hooks/useTheme';
 import { ReactComponent as CoolFace } from 'images/cool.svg';
 import { ReactComponent as HappyFace } from 'images/smile.svg';
 import { ReactComponent as NervousFace } from 'images/eek.svg';
@@ -10,7 +11,7 @@ import { ReactComponent as NervousFace } from 'images/eek.svg';
 const Styles = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
   margin: 0 -2rem;
   padding: 1rem 0.5rem;
   line-height: 1;
@@ -23,16 +24,13 @@ const Styles = styled.div`
     hsla(0, 0%, 0%, 0.05) 2rem
   );
   cursor: default;
-  span:first-child {
+  span:nth-child(1) {
     svg {
       height: 3em;
     }
   }
   span:nth-child(2) {
-    margin: 0 1.5rem;
     font-weight: bold;
-  }
-  span:nth-child(3) {
   }
   ${media.tablet`
     font-size: 1em;
@@ -42,35 +40,36 @@ const Styles = styled.div`
 
 const Animated = animated(Styles);
 
-function Meter({ entropy, theme }) {
-  const { strengthsEnum } = config;
-  const getStrength = bits => {
-    if (bits <= 45) return strengthsEnum.OK;
-    if (bits > 45 && bits <= 60) return strengthsEnum.GOOD;
-    if (bits > 60) return strengthsEnum.STRONG;
-  };
+const { strengthsEnum } = config;
+const strengthGlyphs = {
+  [strengthsEnum.OK]: <NervousFace />,
+  [strengthsEnum.GOOD]: <HappyFace />,
+  [strengthsEnum.STRONG]: <CoolFace />
+};
+
+const getStrength = bits => {
+  if (bits <= 45) return strengthsEnum.OK;
+  if (bits > 45 && bits <= 60) return strengthsEnum.GOOD;
+  return strengthsEnum.STRONG;
+};
+
+export default function Meter({ entropy }) {
+  const theme = useTheme();
   const strength = getStrength(entropy);
-  const emoji =
-    strength === strengthsEnum.OK ? (
-      <NervousFace />
-    ) : strength === strengthsEnum.GOOD ? (
-      <HappyFace />
-    ) : (
-      <CoolFace />
-    );
+  const emoji = strengthGlyphs[strength];
   const backgroundSpring = useSpring({
     config: { duration: 400 },
     backgroundColor: theme.meterColors[strength],
     boxShadow: `0 0 1rem ${theme.meterColors[strength]}`
   });
 
+  entropy = Math.round(entropy);
+
   return (
     <Animated style={backgroundSpring}>
       <span aria-label={strength}>{emoji}</span>
       <span>{strength}</span>
-      <span>~{Math.round(entropy)} bits of entropy</span>
+      <span title={`About ${entropy} bits of entropy`}>~{entropy} bits</span>
     </Animated>
   );
 }
-
-export default withTheme(Meter);
